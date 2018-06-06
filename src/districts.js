854c520cc8bab746a372847c208886ccc95778c7
hk01data <bigdata@hk01.com>
2018-06-06 16:48:40 +0800

    initial commit

---
+++ src/districts.js
@@ -0,0 +1,209 @@
+district_data = [
+	{
+		"district": "港島",
+		"anchors" : [
+			{
+				"anchor": "wanchai",
+				"btn-id": "hk-wc-btn",
+				"display_name": "灣仔",
+				"order": 4,
+				"lat": 22.277516,
+				"lng": 114.173174,
+				"zoom": 18,
+				"mlat": 22.277721,
+				"mlng": 114.172406,
+				"mzoom": 17
+			},
+			{
+				"anchor": "causewaybay",
+				"btn-id": "hk-cwb-btn",
+				"display_name": "銅鑼灣",
+				"order": 5,
+				"lat": 22.279309,
+				"lng": 114.180990,
+				"zoom": "",
+				"mlat": 22.279309,
+				"mlng": 114.180990,
+				"mzoom": ""
+			},
+			{
+				"anchor": "central",
+				"btn-id": "hk-ct-btn",
+				"display_name": "中環",
+				"order": 6,
+				"lat": 22.281300, 
+				"lng": 114.155863,
+				"zoom": "",
+				"mlat": 22.281300,
+				"mlng": 114.155863,
+				"mzoom": 16
+			},
+			{
+				"anchor": "sheungwan",
+				"btn-id": "hk-sw-btn",
+				"display_name": "上環",
+				"order": 7,
+				"lat": 22.285057,
+				"lng": 114.147261,
+				"zoom": "",
+				"mlat": 22.285057,
+				"mlng": 114.147261,
+				"mzoom": 15
+   			},
+            {
+                "anchor": "northpoint",
+                "btn-id": "hk-np-btn",
+                "display_name": "北角",
+                "order": 14,
+                "lat": 22.29074,
+                "lng": 114.198421,
+                "zoom": "",
+                "mlat": 22.29074,
+                "mlng": 114.198421,
+                "mzoom": 15
+            }
+		]
+	},
+	{
+		"district": "九龍",
+		"anchors" : [
+			{
+				"anchor": "mongkok",
+				"btn-id": "kl-mk-btn",
+				"display_name": "旺角",
+				"order": 1,
+				"lat": 22.3193,
+				"lng": 114.169454,
+				"zoom": "",
+				"mlat": 22.31926,
+				"mlng": 114.16932,
+				"mzoom": 16
+			},
+			{
+				"anchor": "princeedward",
+				"btn-id": "kl-pw-btn",
+				"display_name": "太子",
+				"order": 2,
+				"lat": 22.324342,
+				"lng": 114.16828,
+				"zoom": "",
+				"mlat": 22.325195,
+				"mlng": 114.168205,
+				"mzoom": 16
+			},
+			{
+				"anchor": "tsimshatsui",
+				"btn-id": "kl-tst-btn",
+				"display_name": "尖沙咀",
+				"order": 3,
+				"lat": 22.299668, 
+				"lng": 114.174544,
+				"zoom": "",
+				"mlat": 22.299668,
+				"mlng": 114.174544,
+				"mzoom": ""
+			},
+			{
+				"anchor": "kwuntong",
+				"btn-id": "kl-kt-btn",
+				"display_name": "觀塘",
+				"order": 8,
+				"lat": 22.313683,
+				"lng": 114.221671,
+				"zoom": "",
+				"mlat": 22.313683,
+				"mlng": 114.221671,
+				"mzoom": 15
+			},
+			{
+				"anchor": "shamshuipo",
+				"btn-id": "kl-ssp-btn",
+				"display_name": "深水埗",
+				"order": 11,
+				"lat": 22.335666,
+				"lng": 114.159903,
+				"zoom": "",
+				"mlat": 22.335666,
+				"mlng": 114.159903,
+				"mzoom": ""
+			},
+            {
+                "anchor": "hunghum",
+                "btn-id": "kl-hh-btn",
+                "display_name": "紅磡",
+                "order": 15,
+                "lat": 22.3052,
+                "lng": 114.188553,
+                "zoom": "",
+                "mlat": 22.3052,
+                "mlng": 114.188553,
+                "mzoom": 15
+            }
+		]
+	},
+	{
+		"district": "新界",
+		"anchors" : [
+			{
+				"anchor": "tsuenwan",
+				"btn-id": "nt-tw-btn",
+				"display_name": "荃灣",
+				"order": 9,
+				"lat": 22.371299,
+				"lng": 114.115029,
+				"zoom": "",
+				"mlat": 22.371299,
+				"mlng": 114.115029,
+				"mzoom": 15
+			},
+			{
+				"anchor": "tuenmun",
+				"btn-id": "nt-tm-btn",
+				"display_name": "屯門",
+				"order": 10,
+				"lat": 22.398767, 
+				"lng": 113.974384,
+				"zoom": 15,
+				"mlat": 22.395527,
+				"mlng": 113.973965,
+				"mzoom": 14
+			},
+			{
+				"anchor": "taipo",
+				"btn-id": "nt-tp-btn",
+				"display_name": "大埔",
+				"order": 12,
+				"lat": 22.451605,
+				"lng": 114.164757,
+				"zoom": "",
+				"mlat": 22.451605,
+				"mlng": 114.164757,
+				"mzoom": 15
+			},
+			{
+				"anchor": "shatin",
+				"btn-id": "nt-st-btn",
+				"display_name": "沙田",
+				"order": 13,
+				"lat": 22.384838,
+				"lng": 114.189542,
+				"zoom": 15,
+				"mlat": 22.384838,
+				"mlng": 114.189542,
+				"mzoom": 15
+			},
+            {
+                "anchor": "yuenlong",
+                "btn-id": "nt-yl-btn",
+                "display_name": "元朗",
+                "order": 16,
+                "lat": 22.444596,
+                "lng": 114.026879,
+                "zoom": 16,
+                "mlat": 22.444596,
+                "mlng": 114.026879,
+                "mzoom": 15
+            }
+		]
+	}
+]
---
+++ src/event.js
@@ -0,0 +1,95 @@
+{
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.173922,22.272771 ]
+    },
+    "properties": {
+    "id":1,
+    "date":"6月15日",
+    "weekday":"五",
+    "time":"晚上8時",
+    "match":"埃及 VS 烏拉圭",
+    "partner":"聖雅各福群會",
+    "district":"灣仔",
+    "address":"明華堂",
+    "datetime":"2018/6/15  20:00:00"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.944152,22.290497 ]
+    },
+    "properties": {
+    "id":2,
+    "date":"6月16日",
+    "weekday":"六",
+    "time":"晚上9時",
+    "match":"阿根廷 VS 冰島",
+    "partner":"東涌灣岸康樂會",
+    "district":"離島",
+    "address":"東涌社區會堂",
+    "datetime":"2018/6/16  21:00:00"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 113.962056,22.374795 ]
+    },
+    "properties": {
+    "id":3,
+    "date":"6月16日",
+    "weekday":"六",
+    "time":"晚上9時",
+    "match":"阿根廷 VS 冰島",
+    "partner":"蝴蝶邨居民服務協會",
+    "district":"屯門",
+    "address":"房屋署場地",
+    "datetime":"2018/6/16  21:00:00"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.187593,22.384006 ]
+    },
+    "properties": {
+    "id":4,
+    "date":"6月19日",
+    "weekday":"二",
+    "time":"晚上8時",
+    "match":"哥倫比亞 VS 日本",
+    "partner":"沙田青年權益會",
+    "district":"沙田",
+    "address":"排頭村空地",
+    "datetime":"2018/6/19  20:00:00"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 114.230525,22.312594 ]
+    },
+    "properties": {
+    "id":5,
+    "date":"6月19日",
+    "weekday":"二",
+    "time":"晚上8時",
+    "match":"哥倫比亞 VS 日本",
+    "partner":"翠樂陽光居民協會",
+    "district":"觀塘",
+    "address":"翠屏南邨翠櫻樓旁籃球場",
+    "datetime":"2018/6/19  20:00:00"
+    }
+  }
+]
+}
